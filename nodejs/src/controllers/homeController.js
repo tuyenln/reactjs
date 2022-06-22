6aba600bb03411faf4eed3faa124943202735f05
TuyenLN <tuyenlnse@gmail.com>
2022-06-22 14:10:43 +0700

    update controller

--- nodejs/src/controllers/homeController.js
+++ nodejs/src/controllers/homeController.js
@@ -3,9 +3,6 @@ import db from '../models/index';
 let getHomePage = async (req, res) => {
     try {
         let data = await db.User.findAll();
-        console.log('-----------');
-        console.log(data);
-        console.log('-----------');
         return res.render('homepage.ejs', {
             data: JSON.stringify(data)
         });
