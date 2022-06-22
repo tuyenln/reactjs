3d3c5161493ca8d684c76b94b33b2ec4210e4463
TuyenLN <tuyenlnse@gmail.com>
2022-06-22 16:04:29 +0700

    create model, migration, seeder and database table

---
+++ nodejs/src/migrations/migration-create-doctor-clinic-specialty.js
@@ -0,0 +1,33 @@
+'use strict';
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('doctor_clinic_specialty', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      doctorId: {
+        type: Sequelize.INTEGER
+        },
+      clinicId: {
+        type: Sequelize.INTEGER
+        },
+      patientId: {
+        type: Sequelize.INTEGER
+        },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('doctor_clinic_specialty');
+  }
+};
--- nodejs/src/seeders/seeder-users.js
+++ nodejs/src/seeders/seeder-users.js
@@ -13,9 +13,14 @@ module.exports = {
     */
 
     return queryInterface.bulkInsert('Users', [{
-      firstName: 'John',
-      lastName: 'Doe',
-      email: 'example@example.com',
+      email: 'admin@gmail.com',
+      password: '123456',
+      firstName: 'Admin',
+      lastName: 'Super',
+      address: "HN",
+      gender: 1,
+      typeRole: 'ROLE',
+      keyRole: 'R1',
       createdAt: new Date(),
       updatedAt: new Date()
     }]);
